4318e97b545d4646deb52cdd6d998b7a51e00ac3
wowkucko <perenyitamasinvitel@gmail.com>
2018-01-28 17:17:55 +0100

    terkeppage fix

--- src/pages/terkep/terkep.ts
+++ src/pages/terkep/terkep.ts
@@ -1,4 +1,6 @@
-import { TosnapPage } from './../tosnap/tosnap';
+import {
+  TosnapPage
+} from './../tosnap/tosnap';
 import {
   Component
 } from '@angular/core';
@@ -26,7 +28,9 @@ import {
   MarkerOptions,
   Marker
 } from '@ionic-native/google-maps';
-import { LocationAccuracy } from '@ionic-native/location-accuracy';
+import {
+  LocationAccuracy
+} from '@ionic-native/location-accuracy';
 
 /**
  * Generated class for the TerkepPage page.
@@ -44,42 +48,14 @@ export class TerkepPage {
   map: GoogleMap;
   lat: number;
   lang: number;
-  constructor(private locationAccuracy: LocationAccuracy,public loadingCtrl: LoadingController,public modal: ModalController,public locations: HelyekProvider, private toastCtrl: ToastController, private plt: Platform, private geolocation: Geolocation, public navCtrl: NavController) {
+  answer: boolean;
+  constructor(private locationAccuracy: LocationAccuracy, public loadingCtrl: LoadingController, public modal: ModalController, public locations: HelyekProvider, private toastCtrl: ToastController, private plt: Platform, private geolocation: Geolocation, public navCtrl: NavController) {
 
-    this.plt.ready().then(() => {
-      let loader = this.loadingCtrl.create({content: "Helyadatok lekérdezése..."});
-      loader.present();
-      var options = {
-        timeout: 15000
-      };
 
-      this.geolocation.getCurrentPosition(options).then(resp => {
-        this.lat = resp.coords.latitude;
-        this.lang = resp.coords.longitude;
-        loader.dismiss();
-        this.mapLoadingPresent();
-      }).catch(() => {
-        console.log("sikertelen location");
-        this.lat = 47.49801;
-        this.lang = 19.03991;
-        loader.dismiss();
-        this.mapLoadingPresent();
-        this.presentToast();
-      });
-
-    });
   }
   ionViewWillEnter() {
-    this.locationAccuracy.canRequest().then((canRequest: boolean) => {
-      
-        if(canRequest) {
-          this.locationAccuracy.request(this.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY).then(
-            () => console.log('Request successful'),
-            error => console.log('Error requesting location permissions', error)
-          );
-        }
-      
-      });
+    this.locationReq();
+
   }
   presentToast() {
     let toast = this.toastCtrl.create({
@@ -93,80 +69,131 @@ export class TerkepPage {
     });
     toast.present();
   }
+  async locationReq() {
+    const canRequest = await this.locationAccuracy.canRequest();
 
-  mapLoadingPresent(){
-    let loader = this.loadingCtrl.create({content: "Térkép betöltése..."});
+    if (canRequest) {
+      const success = await this.locationAccuracy.request(this.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY).then((success) => {
+        
+        if (success.code==1 || success.code==0){
+          this.answer=true;
+        }
+        else{
+         alert("Hiba történt! Próbáld újra!")
+        }
+      });
+    }
+
+    if (this.answer == true) {
+      console.log('answer is true');
+      await this.plt.ready();
+
+      const loader = this.loadingCtrl.create({
+        content: 'Helyadatok lekérdezése...'
+      });
+      loader.present();
+
+      const options = {
+        timeout: 15000
+      };
+
+      try {
+        const resp = await this.geolocation.getCurrentPosition(options);
+        this.lat = resp.coords.latitude;
+        this.lang = resp.coords.longitude;
+      } catch (err) {
+        this.lat = 47.49801;
+        this.lang = 19.03991;
+        this.presentToast(); // Not sure if this must be awaited..
+      } finally {
+        loader.dismiss();
+        this.mapLoadingPresent();
+      }
+    } else {
+      console.log('answer is no or undefinied');
+    }
+
+  }
+
+
+  mapLoadingPresent() {
+    let loader = this.loadingCtrl.create({
+      content: "Térkép betöltése..."
+    });
     loader.present();
     this.loadGoogleMap().then((x) => {
-        if (x) loader.dismiss();
+      if (x) loader.dismiss();
     });
   }
   loadGoogleMap() {
     return new Promise((resolve) => {
-    let mapOptions: GoogleMapOptions = {
-      camera: {
-        target: {
-          lat: this.lat,
-          lng: this.lang
-        },
-        zoom: 10,
-        tilt: 30
-      }
-    };
-    this.map = GoogleMaps.create('map_canvas', mapOptions);
-
-
-    this.map.one(GoogleMapsEvent.MAP_READY)
-      .then(() => {
-        
-        this.map.addMarker({
-
-          title: 'Jelenlegi helyzet',
-          icon: 'yellow',
-          animation: 'DROP',
-          position: {
+      let mapOptions: GoogleMapOptions = {
+        camera: {
+          target: {
             lat: this.lat,
             lng: this.lang
-          }
-        }).then(() => {
-          
-          let locationsLoaded = this.locations.load().then((result) => {
-            this.map.addMarker({
+          },
+          zoom: 10,
+          tilt: 30
+        }
+      };
+      this.map = GoogleMaps.create('map_canvas', mapOptions);
 
-              title: 'Jelenlegi helyzet',
-              icon: 'yellow',
-              animation: 'DROP',
-              position: {
-                lat: this.lat,
-                lng: this.lang
-              }
-            })
-            
-            let locations = result.locations;
 
-            for (let location of locations) {
+      this.map.one(GoogleMapsEvent.MAP_READY)
+        .then(() => {
+
+          this.map.addMarker({
+
+            title: 'Jelenlegi helyzet',
+            icon: 'yellow',
+            animation: 'DROP',
+            position: {
+              lat: this.lat,
+              lng: this.lang
+            }
+          }).then(() => {
+
+            let locationsLoaded = this.locations.load().then((result) => {
               this.map.addMarker({
 
-                title: location.title,
-                icon: 'red',
+                title: 'Jelenlegi helyzet',
+                icon: 'yellow',
                 animation: 'DROP',
                 position: {
-                  lat: location.latitude,
-                  lng: location.longitude
+                  lat: this.lat,
+                  lng: this.lang
                 }
-              }).then(marker => {
-                marker.on(GoogleMapsEvent.MARKER_CLICK)
-                  .subscribe(() => {
-                    let toSnap = this.modal.create(TosnapPage, { latparams:location.latitude,lngparams:location.longitude });
-                    toSnap.present();
-                  });
-                  
-              });
-              
-            }
+              })
+
+              let locations = result.locations;
+
+              for (let location of locations) {
+                this.map.addMarker({
+
+                  title: location.title,
+                  icon: 'red',
+                  animation: 'DROP',
+                  position: {
+                    lat: location.latitude,
+                    lng: location.longitude
+                  }
+                }).then(marker => {
+                  marker.on(GoogleMapsEvent.MARKER_CLICK)
+                    .subscribe(() => {
+                      let toSnap = this.modal.create(TosnapPage, {
+                        latparams: location.latitude,
+                        lngparams: location.longitude
+                      });
+                      toSnap.present();
+                    });
+
+                });
+
+              }
+            })
           })
-        })
-      });
+        });
       resolve(true);
     })
   }
